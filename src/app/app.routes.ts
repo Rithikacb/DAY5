6284f552c443fda95926e7a2ac4cc7303a13f7ba
Rithika <rithikachittezhath@gmail.com>
2024-08-17 14:52:57 +0530

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProductComponent } from './pages/product/product.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SingleComponent } from './pages/single/single.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:HomeComponent},
+    {path:"about",component:AboutComponent},
+    {path:"product",component:ProductComponent},
+    {path:"login",component:LoginComponent},
+    {path:"signup",component:SignupComponent},
+    {path:"product/:id",component:SingleComponent},
+    {path:"contact",component:ContactComponent},
+    {path:"profile",component:ProfileComponent}
+];
